74614e01c654c2e9fd8e1cbec0d7e7ae074fd171
kiselev.andrey <dWx4awz8t7TPWeR>
2020-10-19 00:08:20 +0300

    остановился на выделении css в отдельный файл

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+console.log(11111111, MiniCssExtractPlugin.loader);
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -32,13 +35,32 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true,
+              reloadAll: true
+            }
+          },
+          'css-loader']
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
