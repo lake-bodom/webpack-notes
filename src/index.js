e5302b313ca6a274d6b10f7a029ae84ef055d059
kiselev.andrey <dWx4awz8t7TPWeR>
2020-10-18 22:11:21 +0300

    различные лоадеры для обработки файлов вебпаком

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Post from './Post';
 import './styles/styles.css';
+import json from './assets/json';
+import WebpackLogo from './assets/webpack-logo.png';
 
-const post = new Post('Webpack post title');
+const post = new Post('Webpack post title', WebpackLogo);
 
 console.log('Post to string', post.toString());
+
+console.log('JSON', json);
